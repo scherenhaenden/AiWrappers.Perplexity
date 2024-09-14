e1184d07c85979012d4586c8360a56e0951d71ad
scherenhaenden <scherenhaenden@gmail.com>
2024-09-14 17:33:35 +0200

    Refactor PerplexityRequestModel to support flexible model input and improve JSON serialization

--- AiWrappers.Perplexity/Core/Models/PerplexityRequestModel.cs
+++ AiWrappers.Perplexity/Core/Models/PerplexityRequestModel.cs
@@ -4,7 +4,31 @@ namespace AiWrappers.Perplexity.Core.Models;
 
 public class PerplexityRequestModel
 {
-    [JsonProperty("model")]
-    public ModelType model { get; set; }
+    [JsonIgnore] // This will hide the enum from JSON serialization
+    public ModelType? model { get; set; }
+
+    // This string stores any model, even if it's not part of the enum
+    private string _modelString;
+
+    [JsonProperty("model")] // This is the exposed string property
+    public string ModelString
+    {
+        get => _modelString ?? model?.ToString(); // Use the raw string or convert enum to string
+        set
+        {
+            _modelString = value; // Always store the string
+
+            // Try to parse the string to the enum, but don't throw an error if it fails
+            if (Enum.TryParse(value, out ModelType parsedModel))
+            {
+                model = parsedModel; // Convert string back to enum if valid
+            }
+            else
+            {
+                model = null; // Invalidate the enum if it's not a known model
+            }
+        }
+    }
+
     public List<Message> messages { get; set; }
 }
