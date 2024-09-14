8fc537d4d0961c3d91e408075f3b57a5481bdc3c
scherenhaenden <scherenhaenden@gmail.com>
2024-09-14 17:34:58 +0200

    Update ModelType enum to include new Perplexity Sonar and Chat models

--- AiWrappers.Perplexity/Core/Models/ModelType.cs
+++ AiWrappers.Perplexity/Core/Models/ModelType.cs
@@ -2,21 +2,27 @@ namespace AiWrappers.Perplexity.Core.Models;
 
 public enum ModelType
 {
-    [StringValue("llama-3-sonar-small-32k-chat")]
-    Llama3SonarSmall32kChat,
+    // Perplexity Sonar Models
+    [StringValue("llama-3.1-sonar-small-128k-online")]
+    Llama31SonarSmall128kOnline,
 
-    [StringValue("llama-3-sonar-small-32k-online")]
-    Llama3SonarSmall32kOnline,
+    [StringValue("llama-3.1-sonar-large-128k-online")]
+    Llama31SonarLarge128kOnline,
 
-    [StringValue("llama-3-sonar-large-32k-chat")]
-    Llama3SonarLarge32kChat,
+    [StringValue("llama-3.1-sonar-huge-128k-online")]
+    Llama31SonarHuge128kOnline,
 
-    [StringValue("llama-3-sonar-large-32k-online")]
-    Llama3SonarLarge32kOnline,
+    // Perplexity Chat Models
+    [StringValue("llama-3.1-sonar-small-128k-chat")]
+    Llama31SonarSmall128kChat,
 
-    [StringValue("llama-3-8b-instruct")]
-    Llama38bInstruct,
+    [StringValue("llama-3.1-sonar-large-128k-chat")]
+    Llama31SonarLarge128kChat,
 
-    [StringValue("llama-3-70b-instruct")]
-    Llama370bInstruct
+    // Open-Source Models
+    [StringValue("llama-3.1-8b-instruct")]
+    Llama318bInstruct,
+
+    [StringValue("llama-3.1-70b-instruct")]
+    Llama3170bInstruct
 }
